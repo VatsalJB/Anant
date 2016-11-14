2ee65ed1a61f0f1b5316dc50266f64cdf4970553
Saurabh Raje <saurabh.mraje@gmail.com>
2016-11-14 20:59:41 +0530

    fixed myapp.c

--- US_FUNCTIONS/myapp.c
+++ US_FUNCTIONS/myapp.c
@@ -6,19 +6,6 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
-int convert(int hex_complement)
-{
-    if(hex_complement&1<<16)        //negative.
-    {
-        int i;
-        for(i = 17; i<33; i++)
-        {
-            hex_complement|=1<<i;
-        }
-    }
-    return hex_complement;
-}
-
 int main()
 {
     /* initialization of userspace function*/
@@ -43,12 +30,10 @@ int main()
     while(1)
     {
         i2c_smbus_write_byte_data(file, 0x02, 0x01);
-        int hex_complement_x = i2c_smbus_read_word_data (file, 0x03);
-        int hex_complement_y = i2c_smbus_read_word_data (file, 0x07);
-        int hex_complement_z = i2c_smbus_read_word_data (file, 0x05);
-        int final_x = convert(hex_complement_x);
-        int final_y = convert(hex_complement_y);
-        int final_z = convert(hex_complement_z);
-        printf("%d %d %d\n", final_x, final_y, final_z);
+        int hex_complement_x = i2c_smbus_read_word_data(file, 0x03);
+        int hex_complement_y = i2c_smbus_read_word_data(file, 0x07);
+        int hex_complement_z = i2c_smbus_read_word_data(file, 0x05);
+        printf("%d %d %d\n", hex_complement_x, hex_complement_y, hex_complement_z);
+        sleep(100);
     }
 }
