aacd6aaac719b92dbdb2ff32d5833f0d625a24b5
smr97 <saurabh.mraje@gmail.com>
2016-12-17 21:13:41 +0530

    Reordered the h/w functions

--- Housekeeping/hwfunctions.h
+++ Housekeeping/hwfunctions.h
@@ -1,72 +1,60 @@
 #ifndef hwfunctions.h
 #define hwfunctions.h
 
-int get_Bx()
+#include "US_FUNCTIONS/userspace.h"
+
+
+float get_S1()
 {
     return rand()%100000;
 }
 
-int get_By()
+float get_S2()
 {
     return rand()%100000;
 }
 
-int get_Bz()
+float get_S3()
 {
     return rand()%100000;
 }
 
-int get_S1()
+float get_S4()
 {
     return rand()%100000;
 }
 
-int get_S2()
+float get_S5()
 {
     return rand()%100000;
 }
 
-int get_S3()
+float get_Wx()
 {
     return rand()%100000;
 }
 
-int get_S4()
+float get_Wy()
 {
     return rand()%100000;
 }
 
-int get_S5()
+float get_Wz()
 {
     return rand()%100000;
 }
 
-int get_Wx()
+float get_Ax()
 {
     return rand()%100000;
 }
 
-int get_Wy()
+float get_Ay()
 {
     return rand()%100000;
 }
 
-int get_Wz()
-{
-    return rand()%100000;
-}
-
-int get_Ax()
-{
-    return rand()%100000;
-}
-
-int get_Ay()
-{
-    return rand()%100000;
-}
-
-int get_Az()
+float get_Az()
 {
     return rand()%100000;
 }
