d61aa5fac8c47c0e44c60f993d94f0cc4c27f6aa
smr97 <saurabh.mraje@gmail.com>
2016-12-18 14:43:39 +0530

    Moved the header

---
+++ US_FUNCTIONS/hwfunctions.h
@@ -0,0 +1,34 @@
+#ifndef hwfunctions.h
+#define hwfunctions.h
+
+#include "US_FUNCTIONS/GY521.h"
+#include "US_FUNCTIONS/HMC5883L.h"
+
+float get_S1()
+{
+    return rand()%100000;
+}
+
+float get_S2()
+{
+    return rand()%100000;
+}
+
+float get_S3()
+{
+    return rand()%100000;
+}
+
+float get_S4()
+
+{
+    return rand()%100000;
+}
+
+float get_S5()
+{
+    return rand()%100000;
+}
+
+
+#endif
