fd2f227529838c64ad351f78c83679636efc39fc
dvjsm26 <dhananjayvs.m@gmail.com>
2016-12-19 22:33:50 +0000

    Added init and clear

--- US_FUNCTIONS/GY521.c
+++ US_FUNCTIONS/GY521.c
@@ -22,7 +22,7 @@ void set_sleep_gy521(int flag)
 		uint8_t val8 = 0x01;     //write 0x00 if you want to set the internal 8MHz oscillator as CLK                                              
 		res = i2c_smbus_write_byte_data(file, reg, val8);
 		if(res<0) 
-			perror("Failed to wake it up\n");
+			perror("Failed to wake it up");
 		/*else 
 			printf("Device is awake\n");
 		*/
@@ -33,7 +33,7 @@ void set_sleep_gy521(int flag)
 		uint8_t val8 = 0x41;   //write 0x40 if you want to set the internal 8MHz oscillator as CLK
 		res = i2c_smbus_write_byte_data(file, reg, val8);
 		if(res<0) 
-			perror("Failed to go to sleep\n");
+			perror("Failed to go to sleep");
 		/*else 
 			printf("In sleep mode\n");
 		*/
@@ -53,7 +53,7 @@ void init_gy521()
 	}
 	if(ioctl(file, I2C_SLAVE, ADDR)<0)
 	{
-		perror(":( Not able to access the device\n");
+		perror("Not able to access the device");
 		exit(EXIT_FAILURE);
 	}
 
@@ -61,10 +61,10 @@ void init_gy521()
 
 	res = i2c_smbus_write_byte_data(file, 0x1B, 0x00);
 	if(res<0)
-		perror("Failed to set gyro range\n");
+		perror("Failed to set gyro range");
 	res = i2c_smbus_write_byte_data(file, 0x1C, 0x00);
 	if(res<0)
-		perror("Failed to set the accelerometer range\n");
+		perror("Failed to set the accelerometer range");
 
 	set_sleep_gy521(0);  //this also sets the clock source to X-axis gyro reference which is slightly better than the internal 8MHz oscillator
 }
@@ -73,41 +73,56 @@ float get_Ax()
 {
 	int16_t xout = i2c_smbus_read_byte_data(file, 0x3B); int16_t xout1 = i2c_smbus_read_byte_data(file, 0x3C);
 	xout = (xout<<8) | xout1;
-	return xout/16384.0*9.8;
+	return xout/16384.0*9.8;	//taking value of g=9.8
 }
+
 float get_Ay()
 {	
 	int16_t yout = i2c_smbus_read_byte_data(file, 0x3D); int16_t yout1 = i2c_smbus_read_byte_data(file, 0x3E);
 	yout = (yout<<8) | yout1;
 	return yout/16384.0*9.8;
 }
+
 float get_Az()
 {
 	int16_t zout = i2c_smbus_read_byte_data(file, 0x3F); int16_t zout1 = i2c_smbus_read_byte_data(file, 0x40);
 	zout = (zout<<8) | zout1;
 	return zout/16384.0*9.8;
 }
+
 float get_temp()
 {
-	__s16 temp = i2c_smbus_read_byte_data(file, 0x41); __s16 temp1 = i2c_smbus_read_byte_data(file, 0x42);
-	temp = (temp<<8) | temp1;
-	return (temp/340.0 + 36.53);
+	res = i2c_smbus_read_word_data(file, 0x41);           					//finally!
+	__s32 lsb = (res & 0x0000ff00); lsb=(lsb>>8)&0x000000ff;
+	__s32 msb = (res & 0x000000ff); msb=msb<<8;
+	__s32 temp = msb|lsb; 
+	temp=temp<<16; temp=temp>>16;
+	return (temp/340.0 + 36.53);	
 }
+
 float get_Wx()
 {
 	__s16 gyro_x = i2c_smbus_read_byte_data(file, 0x43); __s16 gyro_x1 = i2c_smbus_read_byte_data(file, 0x44);
 	gyro_x = (gyro_x<<8) | gyro_x1;	
 	return gyro_x/131.0;
 }
+
 float get_Wy()
 {
 	__s16 gyro_y = i2c_smbus_read_byte_data(file, 0x45); __s16 gyro_y1 = i2c_smbus_read_byte_data(file, 0x46);
 	gyro_y = (gyro_y<<8) | gyro_y1;
 	return gyro_y/131.0;
 }
+
 float get_Wz()
 {
 	__s16 gyro_z = i2c_smbus_read_byte_data(file, 0x47); __s16 gyro_z1 = i2c_smbus_read_byte_data(file, 0x48);
 	gyro_z = (gyro_z<<8) | gyro_z1;
 	return gyro_z/131.0;
 }
+
+void clear_gy521()
+{	
+	close(file);
+}
+
