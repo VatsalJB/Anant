919ae598009a88450988672b01675994321b4478
VatsalJB <vatsaljb.007@gmail.com>
2017-04-06 18:34:56 +0530

    Added new file

--- CAM/TAKE_PIC.c
+++ CAM/TAKE_PIC.c
@@ -7,10 +7,10 @@
 //signal handler for a custom real time signal that is generated iff the camera is not taking a picture.
 //this signal handler should suicide.
 
-void respond(void)
+void respond(int i)                 //change by VJB (function argument changed to int from void)
 {
     //printf("Signal received\n");
-    exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);     // i is just used to match the prototype of function pointer sa_handler
 }
 
 int check_power()
@@ -30,7 +30,7 @@ void take_pic(void)
 int main()
 {
     struct sigaction handlr_struct_rt;
-    handlr_struct_rt.sa_handler = respond;
+    handlr_struct_rt.sa_handler = respond; 			 //change by VJB (function pointer data type mismatch)
     sigaction(CAMSIG, &handlr_struct_rt, NULL);
     int flag = check_power();
     if(flag==0)
